b0a326f1b5a68c1e725ebcc99e3d301bb167a167
heap-s <dqkushbro@gmail.com>
2024-07-21 05:41:03 -0700

    Init local

---
+++ CT-1/main.java
@@ -0,0 +1,9 @@
+class Descriptor{
+    public static void main(String[] args){
+        System.out.println("First Name: Andy");
+        System.out.println("Last Name: Miller");
+        System.out.println("333 Strawberry Ln");
+        System.out.println("Geographic Township, Narnia");
+        System.out.println("31245");
+    }
+} 
