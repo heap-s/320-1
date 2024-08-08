65a4bb52e045f3879e441fc13e44bb0624695517
heap-s <dqkushbro@gmail.com>
2024-08-08 14:42:31 -0700

    Commit loops

---
+++ DF/Loops.java
@@ -0,0 +1,16 @@
+package DF;
+
+public class Loops {
+    public static void main(String[] args) {
+        int x = 1;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(x++);
+        }
+
+        int a = 1;
+        while (a <= 10) {
+            System.out.println(a++);
+        }
+    }
+}
